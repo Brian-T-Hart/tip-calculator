1a524568c9e5aa0d1fa2bdb61a04e1c7754dcb7e
Brian-T-Hart <hart.briant@gmail.com>
2019-05-10 12:25:44 -0700

    added methods to handle button clicks

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ var totalTip;
 var splitWays = 1;
 var splitTip;
 var tipElement = document.getElementById('total-tip');
+var tipInput = document.getElementById('tip-input');
+var tipInputContainer = document.getElementById('tip-input-container');
 var splitTipElement = document.getElementById('split-tip');
-var splitWaysElement = document.getElementById('split-ways');
 var costElement = document.getElementById('total-cost');
 var percentElement = document.getElementById('tip-percent');
 
 function calculateTip(cost, percent) {
-	console.log(cost, percent);
 	if (cost >= 0 && percent >= 0) {
-		console.log("unrounded tip " + (cost * percent / 100));
 		totalTip = parseFloat(Math.round(cost * percent) / 100).toFixed(2);
 		tipElement.textContent = "Total Tip: $" + totalTip;
 	} else {
@@ -21,10 +20,9 @@ function calculateTip(cost, percent) {
 }
 
 function calculateSplitTip(tipTotal, numOfPeople) {
-	console.log('calculateSplitTip', tipTotal, numOfPeople);
 	if (numOfPeople > 1 && totalCost > 0 && tipPercent > 0) {
 		splitTip = parseFloat(Math.ceil(tipTotal * 100 / numOfPeople) / 100).toFixed(2);
-		splitTipElement.textContent = "Split Tip: $" + splitTip + " per person";
+		splitTipElement.textContent = "Split " + numOfPeople + " ways: $" + splitTip + " per person";
 	} else {
 		splitTipElement.textContent = "";
 	}
@@ -32,7 +30,6 @@ function calculateSplitTip(tipTotal, numOfPeople) {
 
 function setTotalCost() {
 	totalCost = parseFloat(event.target.value).toFixed(2);
-	console.log("total cost: " + totalCost);
 	if (totalCost >= 0) {
 		costElement.textContent = "Total Cost: $" + totalCost;
 	} else {
@@ -43,8 +40,14 @@ function setTotalCost() {
 	calculateSplitTip(totalTip, splitWays);
 }
 
-function setTip() {
-	tipPercent = parseInt(event.target.value);
+function setTip(tip) {
+	let element = event.target;
+	if (tip) {
+		tipPercent = tip;
+	} else {
+		tipPercent = parseInt(element.value);
+	}
+
 	if (tipPercent >= 0) {
 		percentElement.textContent = "Tip Percent: " + tipPercent + "%";
 	} else {
@@ -56,13 +59,39 @@ function setTip() {
 }
 
 function setSplitWays() {
-	console.log(event.target.value);
 	splitWays = parseInt(event.target.value);
-	if (splitWays > 1) {
-		splitWaysElement.textContent = "Tip split " + splitWays + " ways";
-	} else {
-		splitWaysElement.textContent = "";
-	}
-
 	calculateSplitTip(totalTip, splitWays);
+}
+
+function toggleElement(el) {
+	var element = document.getElementById(el);
+	element.classList.contains('hidden') ? element.classList.remove('hidden') : element.classList.add('hidden');
+}
+
+function handleCircleBorder(element) {
+	var elements = document.querySelectorAll('.circle');
+	for (let i = 0; i < elements.length; i++) {
+		if (elements[i].classList.contains('focused')) {
+			elements[i].classList.remove('focused');
+		}
+	}
+	element.classList.add('focused');
+}
+
+function handleDefaultTip(tip) {
+	let element = event.target;
+	handleCircleBorder(element);
+	setTip(tip);
+	if (!tipInputContainer.classList.contains('hidden')) {
+		tipInput.value = '';
+		toggleElement(tipInputContainer.id);
+	}
+}
+
+function handleOther() {
+	let element = event.target;
+	handleCircleBorder(element);
+	if (tipInputContainer.classList.contains('hidden')) {
+		toggleElement(tipInputContainer.id);
+	}
 }
