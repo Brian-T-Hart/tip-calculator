3e6b915c8cbeab98e434d880067344842f2c7fdb
Brian-T-Hart <hart.briant@gmail.com>
2019-05-17 09:18:15 -0700

    added eventListener for tab key and organized code

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
-var totalCost;
-var tipPercent;
-var totalTip;
+var costElement = document.getElementById('total-cost');
+var percentElement = document.getElementById('tip-percent');
 var splitWays = 1;
 var splitTip;
-var tipElement = document.getElementById('total-tip');
-var tipInput = document.getElementById('tip-input');
-var tipInputContainer = document.getElementById('tip-input-container');
 var splitTipElement = document.getElementById('split-tip');
 var splitWaysContainer = document.getElementById('split-ways-container');
 var splitWaysInput = document.getElementById('split-ways-input');
-var costElement = document.getElementById('total-cost');
-var percentElement = document.getElementById('tip-percent');
+var tabPressed = false;
+var tipElement = document.getElementById('total-tip');
+var tipInput = document.getElementById('tip-input');
+var tipInputContainer = document.getElementById('tip-input-container');
+var tipPercent;
+var totalCost;
+var totalTip;
 
-function validateInput(input, type) {
-	var regex;
-	var valid;
-	if (type === 'float') {
-		regex = /^[0-9]\d*(?:\.\d{0,2})?$/;
-		valid = regex.test(input);
+document.addEventListener('keyup', checkTabPress);
+
+function calculateSplitTip(tipTotal, numOfPeople) {
+	if (numOfPeople > 1 && totalCost > 0 && tipPercent > 0) {
+		splitTip = parseFloat(Math.ceil(tipTotal * 100 / numOfPeople) / 100).toFixed(2);
+		splitTipElement.textContent = "$" + splitTip + " each";
+	} else {
+		splitTipElement.textContent = "";
 	}
-	if (type === 'integer') {
-		regex = /^\d+$/;
-		valid = regex.test(input);
-	}
-	
-	if (input.length === 0) {
-		valid = true;
-	}
-	if (input.length > 15) {
-		valid = false;
-	}
-	return valid;
 }
 
 function calculateTip(cost, percent) {
@@ -42,75 +33,16 @@ function calculateTip(cost, percent) {
 	}
 }
 
-function calculateSplitTip(tipTotal, numOfPeople) {
-	if (numOfPeople > 1 && totalCost > 0 && tipPercent > 0) {
-		splitTip = parseFloat(Math.ceil(tipTotal * 100 / numOfPeople) / 100).toFixed(2);
-		splitTipElement.textContent = "$" + splitTip + " each";
-	} else {
-		splitTipElement.textContent = "";
+// open hidden inputs on tab press to make acccessible with keyboard only
+function checkTabPress() {
+	if (event.keyCode === 9 && !tabPressed) {
+		tabPressed = true;
+		tipInputContainer.classList.remove('hidden');
+		splitWaysContainer.classList.remove('hidden');
 	}
 }
 
-function setTotalCost() {
-	let element = event.target;
-	let validated = validateInput(element.value, 'float');
-	if (validated) {
-		element.classList.contains('error') ? element.classList.remove('error') : null;
-		totalCost = parseFloat(element.value).toFixed(2);
-		if (totalCost >= 0) {
-			costElement.textContent = "Bill: $" + totalCost;
-		} else {
-			costElement.textContent = "";
-		}
-
-		calculateTip(totalCost, tipPercent);
-		calculateSplitTip(totalTip, splitWays);
-	} else {
-		element.classList.add('error');
-	}
-}
-
-function setTip(tip) {
-	let element = event.target;
-		if (tip) {
-			tipPercent = tip;
-		} else {
-			let validated = validateInput(element.value, 'integer');
-			if (validated) {
-				element.classList.contains('error') ? element.classList.remove('error') : null;
-				tipPercent = parseInt(element.value);
-			} else {
-				element.classList.add('error');
-			}
-		}
-
-		if (tipPercent >= 0) {
-			percentElement.textContent = "@ " + tipPercent + "%";
-		} else {
-			percentElement.textContent = "";
-		}
-		
-		calculateTip(totalCost, tipPercent);
-		calculateSplitTip(totalTip, splitWays);
-}
-
-function setSplitWays() {
-	let element = event.target;
-	let validated = validateInput(element.value, 'integer');
-	if (validated) {
-		element.classList.contains('error') ? element.classList.remove('error') : null;
-		splitWays = parseInt(event.target.value);
-		calculateSplitTip(totalTip, splitWays);
-	} else {
-		element.classList.add('error');
-	}
-}
-
-function toggleElement(el) {
-	var element = document.getElementById(el);
-	element.classList.contains('hidden') ? element.classList.remove('hidden') : element.classList.add('hidden');
-}
-
+// add border color to circle when clicked
 function handleCircleBorder(element) {
 	var elements = document.querySelectorAll('.circle');
 	for (let i = 0; i < elements.length; i++) {
@@ -144,4 +76,85 @@ function handleSplitIt() {
 	if (!splitWaysContainer.classList.contains('hidden')) {
 		splitWaysInput.focus();
 	}
+}
+
+function setTip(tip) {
+	let element = event.target;
+		if (tip) {
+			tipPercent = tip;
+		} else {
+			let validated = validateInput(element.value, 'integer');
+			if (validated) {
+				element.classList.contains('error') ? element.classList.remove('error') : null;
+				tipPercent = parseInt(element.value);
+			} else {
+				element.classList.add('error');
+			}
+		}
+
+		if (tipPercent >= 0) {
+			percentElement.textContent = "@ " + tipPercent + "%";
+		} else {
+			percentElement.textContent = "";
+		}
+		
+		calculateTip(totalCost, tipPercent);
+		calculateSplitTip(totalTip, splitWays);
+}
+
+function setTotalCost() {
+	let element = event.target;
+	let validated = validateInput(element.value, 'float');
+	if (validated) {
+		element.classList.contains('error') ? element.classList.remove('error') : null;
+		totalCost = parseFloat(element.value).toFixed(2);
+		if (totalCost >= 0) {
+			costElement.textContent = "Bill: $" + totalCost;
+		} else {
+			costElement.textContent = "";
+		}
+
+		calculateTip(totalCost, tipPercent);
+		calculateSplitTip(totalTip, splitWays);
+	} else {
+		element.classList.add('error');
+	}
+}
+
+function setSplitWays() {
+	let element = event.target;
+	let validated = validateInput(element.value, 'integer');
+	if (validated) {
+		element.classList.contains('error') ? element.classList.remove('error') : null;
+		splitWays = parseInt(event.target.value);
+		calculateSplitTip(totalTip, splitWays);
+	} else {
+		element.classList.add('error');
+	}
+}
+
+function toggleElement(el) {
+	var element = document.getElementById(el);
+	element.classList.contains('hidden') ? element.classList.remove('hidden') : element.classList.add('hidden');
+}
+
+function validateInput(input, type) {
+	var regex;
+	var valid;
+	if (type === 'float') {
+		regex = /^[0-9]\d*(?:\.\d{0,2})?$/;
+		valid = regex.test(input);
+	}
+	if (type === 'integer') {
+		regex = /^\d+$/;
+		valid = regex.test(input);
+	}
+	
+	if (input.length === 0) {
+		valid = true;
+	}
+	if (input.length > 15) {
+		valid = false;
+	}
+	return valid;
 }
